d67d074a7143850aac5cd13338a4d1b15b697989
ZianTsabit <13520165@std.stei.itb.ac.id>
2023-12-15 18:08:17 +0700

    finish API

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# bansosplus-ml
-Repository for credit scoring model bansos plus
+# BansosPlus Credit Scoring Model
+
+Bansos Plus using credit scoring model to assess whether someone deserves to get the Bansos or not
+
+### How to Build and Run?
+
+1. create a python virtual environtment using this command below.
+
+   ```
+   python3 -m venv .env
+   source .venv/bin/activate
+   ```
+2. Install all the requirements using this command below.
+
+   ```
+   pip install -r requirements.txt
+   ```
+3. Use this command to run the API
+
+   ```
+   uvicorn api:app --reload
+   ```
+
+The API will run on port 8000.
--- api.py
+++ api.py
@@ -1,4 +1,7 @@
 import requests
+import tensorflow as tf
+import pandas as pd
+import numpy as np
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,6 +23,60 @@ def return_format(code, message, data, is_success=True):
         'data': ret_data
     }
 
-@app.get("/api/credit_scoring")
-def main():
+def load_and_preprocess_model(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+def predict_bansos(interpreter, input_data,):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    input_data = np.array(input_data, dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    return predictions
+
+@app.get("/")
+def root():    
     return return_format(200, 'Successfully doing test', 'Hello World')
+
+@app.post("/api/predict_new_user")
+def predict_new_user(bansos_registration_id : int, penghasilan : int, jumlah_makan : int, berobat : int, tanggungan : int, bahan_bakar : int, jumlah_aset : int, luas_lantai : int, jenis_dinding : int, pendidikan: int):
+    
+    new_data = pd.DataFrame({
+        'penghasilan': [int(penghasilan)],
+        'jumlah_makan': [int(jumlah_makan)],
+        'berobat': [int(berobat)],
+        'tanggungan': [int(tanggungan)],
+        'bahan_bakar': [int(bahan_bakar)],
+        'jumlah_aset': [int(jumlah_aset)],
+        'luas_lantai': [int(luas_lantai)],
+        'jenis_dinding': [int(jenis_dinding)],
+        'pendidikan': [int(pendidikan)],
+    }, index=[0])
+
+    tflite_model_path = "model.tflite"  # Replace with the path to your .tflite file
+    loaded_tflite_model = load_and_preprocess_model(tflite_model_path)
+
+    predictions = predict_bansos(loaded_tflite_model, new_data)
+    print(predictions)
+    score = float(predictions[0])
+    print(score)
+    decisions = {}
+    
+    if score >= 0.75:
+        decisions['id'] = bansos_registration_id
+        decisions['score'] = round(score, 4)
+        decisions['status'] = "ACCEPTED"
+        x = requests.put(f'http://35.202.238.22:8001/api/bansos-registration/accept?bansos_registration_id={bansos_registration_id}')
+
+    else:
+        decisions['id'] = bansos_registration_id
+        decisions['score'] = round(score, 4)
+        decisions['status'] = "REJECTED"
+        x = requests.put(f'http://35.202.238.22:8001/api/bansos-registration/reject?bansos_registration_id={bansos_registration_id}')  
+
+    return return_format(200, 'Successfully doing prediction for new user', decisions)
